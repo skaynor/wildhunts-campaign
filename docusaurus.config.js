fa66a8cefee2ef1cc82745556175f47b269f0172
skaynor <samkaynor@gmail.com>
2024-06-10 10:34:33 -0500

    github pages config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,11 +17,12 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'skaynor', // Usually your GitHub org/user name.
+  projectName: 'wildhunts-campaign', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
