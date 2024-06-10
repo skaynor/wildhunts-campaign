265a88ab4784daeb57a2162eeb85dda7ca30de51
skaynor <samkaynor@gmail.com>
2024-06-10 11:04:04 -0500

    src dir updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Resources - Getting Started
           </Link>
         </div>
       </div>
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Home`}
+      description="Wild Hunts Campaign">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
